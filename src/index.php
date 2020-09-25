f14ae04ec99db83ae41d12ae5eba179047d15b68
Jorrit van Geffen <jorritvgeffen@gmail.com>
2020-09-26 01:53:41 +0200

    php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,7 @@
-<?php
-require_once '../vendor/autoload.php';
-
-require_once 'FormHandler.php';
+<?php     
+$to_email = 'julian.oudenalder@gmail.com';
+$subject = 'Contactformulier';
+$message = 'Dit bericht is verstuurd door middel van het contactformulier op ThePcLab.nl';
+$headers = 'From: mail@test.com';
+mail($to_email,$subject,$message,$headers);
+?>
