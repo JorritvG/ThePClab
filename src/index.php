44557d9e362f01ef762f7867c90f966cc5efee7c
Jorrit van Geffen <jorritvgeffen@gmail.com>
2020-09-28 21:57:31 +0200

    php weg

--- src/index.php
+++
@@ -1,7 +0,0 @@
-<?php     
-$to_email = 'julian.oudenalder@gmail.com';
-$subject = 'Contactformulier';
-$message = 'Dit bericht is verstuurd door middel van het contactformulier op ThePcLab.nl';
-$headers = 'From: mail@test.com';
-mail($to_email,$subject,$message,$headers);
-?>
